ce42e0c25a588040dc9ba40397431fc5999da5a3
jazarelly <jazarelly_zapata@usmp.pe>
2026-04-30 10:44:21 -0500

    Dtos, crear despacho y empresa busqueda

---
+++ DTOs/EmpresaBusquedaDto.cs
@@ -0,0 +1,9 @@
+namespace LogisticaBroker.DTOs
+{
+    public class EmpresaBusquedaDto
+    {
+        public int IdEmpresa { get; set; }
+        public string Ruc { get; set; } = string.Empty;
+        public string RazonSocial { get; set; } = string.Empty;
+    }
+}
